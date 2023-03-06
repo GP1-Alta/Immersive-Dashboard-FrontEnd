2b0c779522993473d5e971e3c3a4e4a5a856c449
arbi kustia <arbikustia14@gmail.com>
2023-03-06 16:32:55 +0700

    login without api

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': '#F47522',
+        'blue': '#19345E',
+        'grayalta': '#EFF0F2' 
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui : {
