12dd1e2b380603ae6854638669e9527fa04fdf81
Gilang Ramadhan <gilangr1440@gmail.com>
2023-03-10 00:59:22 +0700

    add log

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { FC } from "react";
 
-const Card = () => {
+interface CardProps {
+  status?: string;
+  feedback?: string;
+}
+
+const Card: FC<CardProps> = ({ status, feedback }) => {
   return (
     <div className="mx-16 my-10 p-8 rounded-lg shadow-md">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vero deserunt odit veritatis natus labore impedit doloremque nisi, laboriosam animi dolore iure cum, quisquam amet dolores iusto fugiat recusandae assumenda.
-      <h1>Changed Status: Continue Session 2</h1>
+      <p className="my-5">{feedback}</p>
+      <h1>Changed Status: {status}</h1>
     </div>
   );
 };
--- src/pages/MenteeLog.tsx
+++ src/pages/MenteeLog.tsx
@@ -10,10 +10,15 @@ const MenteeLog = () => {
   const { id_user } = useParams();
   const [cookies, setCookie] = useCookies<any>(["id", "token", "username"]);
   const [mentee, setMentee] = useState<MenteeType[]>([]);
+  const [dataStatus, setDataStatus] = useState<any>([]);
+  const [dataLog, setDataLog] = useState<any>([]);
+  const [status, setStatus] = useState<any>();
+  const [feedback, setFeedback] = useState<string>("");
+  const idStatus = parseInt(status);
 
   const token = cookies.token;
 
-  function getMentee() {
+  useEffect(() => {
     axios
       .get(`https://altaimmersive.site/mentees/${id_user}`, {
         headers: {
@@ -21,33 +26,73 @@ const MenteeLog = () => {
         },
       })
       .then((response) => {
-        setMentee(response.data.data);
+        const data = [];
+        data.push(response.data.data);
+        setMentee(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`https://altaimmersive.site/status`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.data);
+        setDataStatus(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`https://altaimmersive.site/mentees/${id_user}/logs`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.data);
+        setDataLog(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const addNewLog = () => {
+    axios
+      .post(
+        `https://altaimmersive.site/mentees/${id_user}/logs`,
+        {
+          status_id: idStatus,
+          feedback: `${feedback}`,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
         console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
-  useEffect(() => {
-    getMentee();
-  }, []);
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`https://altaimmersive.site/mentees/${id_user}`, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     })
-  //     .then((response) => {
-  //       setMentee(response.data.data);
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
+  const clearData = () => {
+    setStatus("");
+    setFeedback("");
+  };
 
   console.log(mentee);
 
@@ -55,31 +100,86 @@ const MenteeLog = () => {
     <div>
       <Layout>
         <div className="mx-16 my-10">
-          {mentee.map((data: any, index: any) => {
+          {mentee?.map((data: any, index: any) => {
             return (
               <table className="w-full">
                 <tr>
                   <td className="w-2/3 text-xl">{data.name}</td>
-                  <td>Phone: 0819839649202</td>
+                  <td>Phone: {data.phone}</td>
                 </tr>
                 <tr>
-                  <td className="w-2/3">Quality Engineer Batch 8</td>
-                  <td>Telegram: @rachmankamil</td>
+                  <td className="w-2/3">{data.class}</td>
+                  <td>Telegram: {data.telegram}</td>
                 </tr>
                 <tr>
-                  <td className="w-2/3">IPA</td>
-                  <td>Discord: @rachmankamil#2347</td>
+                  <td className="w-2/3">{data.major}</td>
+                  <td>Discord: {data.discord}</td>
                 </tr>
                 <tr>
-                  <td className="w-2/3">SMA Negeri 4 Surabaya</td>
-                  <td>Email: rachmankamil24@gmail.com</td>
+                  <td className="w-2/3">{data.institution}</td>
+                  <td>Email: {data.email}</td>
                 </tr>
               </table>
             );
           })}
         </div>
+        <div className="flex justify-end my-2 mr-16">
+          <label htmlFor="my-modal-4" className="btn bg-[#19345E]">
+            Add New
+          </label>
+        </div>
+        {dataLog.map((data: any) => (
+          <Card status={data.status} feedback={data.feedback} />
+        ))}
 
-        <Card />
+        <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+        <label htmlFor="my-modal-4" className="modal cursor-pointer">
+          <label className="modal-box relative w-screen" htmlFor="">
+            <h3 className="text-lg font-bold text-center mb-5">Add New Log</h3>
+            <form action="">
+              <table className="w-full">
+                <tr>
+                  <td>
+                    <label className="label">
+                      <span className="label-text">Status</span>
+                    </label>
+                  </td>
+                  <td>
+                    <select value={status} onChange={(e) => setStatus(e.target.value)} className="mb-3 select select-bordered w-full max-w-xs">
+                      {dataStatus?.map((data: any, index: any) => {
+                        return (
+                          <option key={index} value={data.id}>
+                            {data.name}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <label className="label">
+                      <span className="label-text">Feedback</span>
+                    </label>
+                  </td>
+                  <td>
+                    <textarea value={feedback} onChange={(e) => setFeedback(e.target.value)} className="textarea textarea-bordered w-full" placeholder="Feedback"></textarea>
+                  </td>
+                </tr>
+              </table>
+              <div className="grid justify-items-center w-full">
+                <div className="flex w-full gap-5 justify-end">
+                  <label onClick={clearData} htmlFor="my-modal-4" className="btn bg-white border-[#19345E] text-[#19345E] hover:bg-[#19345E] hover:text-white w-20 flex gap-2">
+                    Cancel
+                  </label>
+                  <label onClick={addNewLog} htmlFor="my-modal-4" className="btn bg-[#19345E] w-20 flex gap-2">
+                    Save
+                  </label>
+                </div>
+              </div>
+            </form>
+          </label>
+        </label>
       </Layout>
     </div>
   );
